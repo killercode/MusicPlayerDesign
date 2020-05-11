afd8ef845852c7a45e452719acfe26b5e2553c21
ricky-codes <ricardu12345@gmail.com>
2020-05-11 12:17:49 +0100

    Add project files.

---
+++ Views/MusicListView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayerDesign.Views
+{
+    /// <summary>
+    /// Interaction logic for MusicListView.xaml
+    /// </summary>
+    public partial class MusicListView : Page
+    {
+        public MusicListView()
+        {
+            InitializeComponent();
+
+            List<TodoItem> items = new List<TodoItem>();
+            items.Add(new TodoItem() { Title = "Complete this WPF tutorial", Completion = 45 });
+        }
+
+        public class TodoItem
+        {
+            public string Title { get; set; }
+            public int Completion { get; set; }
+        }
+    }
+}
